a45c23ef3eb6692daa637328f6266ca6105ac01b
iqbalsalik <sisalik84@gmail.com>
2023-03-08 00:30:56 +0530

    second official commit

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ header.style.borderBottom = "3px solid black"
 const addItem = document.querySelector("h2");
 addItem.style.fontWeight = "bold"
 addItem.style.color = "green"
+
+const items = document.getElementsByClassName("list-group-item");
+
+items[2].style.backgroundColor = "green"
+
+for(let i =0;i<items.length;i++){
+    items[i].style.fontWeight = "bold"
+    items[i].style.color = "red"
+}
