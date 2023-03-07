046f5a079953795fc81e2a6c2666ac18f1334f9c
iqbalsalik <sisalik84@gmail.com>
2023-03-07 23:47:04 +0530

    first official commit

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 console.log("first git project")
-let a  = new Map()
-console.log('something')
+
+console.log(document.title)
+document.title = "someThing else"
+console.log(document.title)
+const header = document.getElementById("main-header")
+header.style.borderBottom = "3px solid black"
+const addItem = document.querySelector("h2");
+addItem.style.fontWeight = "bold"
+addItem.style.color = "green"
