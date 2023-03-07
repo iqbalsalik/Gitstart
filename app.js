a94e77ff7594276d869048f43f654e920511dbc1
iqbalsalik <sisalik84@gmail.com>
2023-03-08 00:03:05 +0530

    first branch commit

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ header.style.borderBottom = "3px solid black"
 const addItem = document.querySelector("h2");
 addItem.style.fontWeight = "bold"
 addItem.style.color = "green"
+const add = document.getElementById("add");
+const btn = document.getElementById("btn");
+const li = document.querySelectorAll("li");
+let count = 0;
+let number = 1;
+btn.addEventListener("click",(e)=>{
+e.preventDefault()
+if(add.value == ''){
+    alert("Please fill the item")
+}else{
+    li[count].innerText = `item ${number} : ${add.value}`
+    count++
+    number++
+    add.value = ''
+    if(count>3 && number>4){
+        count = 0;
+        number = 1;
+    }
+}
+})
