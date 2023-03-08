9fb195b4671c8248d739de76048dfdc31eaaac64
iqbalsalik <sisalik84@gmail.com>
2023-03-08 16:31:40 +0530

    sixth commit

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
 let item = document.getElementById("item");
 const submitBtn = document.getElementById("submitBtn")
 const itemList = document.getElementById("items")
+const filter = document.getElementById("filter");
+const itemDescription = document.getElementById("description")
 
+submitBtn.addEventListener("click", addItem);
 
-submitBtn.addEventListener("click", e => {
+itemList.addEventListener("click", removeItem);
+
+filter.addEventListener("keyup",filterItems)
+
+function addItem(e){
     e.preventDefault();
+    //creating li element
     let newLi = document.createElement("li");
+    // adding className
     newLi.className = "list-group-item"
+    // creating deleteBtn
     let deleteBtn = document.createElement('button');
     deleteBtn.className = "btn btn-danger btn-sm float-right delete"
     deleteBtn.innerText = "X"
+
+    //creating edit button
     let editBtn = document.createElement("button");
     editBtn.className = "btn btn-primary btn-sm float-right edit ml-1"
     editBtn.appendChild(document.createTextNode("edit"))
+    // appendChild to created li
     newLi.innerText = item.value;
     newLi.appendChild(editBtn)
     newLi.appendChild(deleteBtn);
     itemList.appendChild(newLi)
     item.value = ''
-});
 
-itemList.addEventListener("click", removeItem);
+    let descriptionText = document.createTextNode(" " + itemDescription.value)
+    newLi.appendChild(descriptionText)
+    itemDescription.value = ''
+}
 
 function removeItem(e) {
     if (e.target.classList.contains("delete")) {
         let li = e.target.parentElement;
         itemList.removeChild(li)
     }
+}
+
+function filterItems(e){
+   let filterText =  e.target.value.toLowerCase();
+   let itemText = document.querySelectorAll("li")
+//    Array.from(itemText).forEach(item=>{
+//     console.log(item.firstChild.nextSibling)
+//     // if(item.firstChild.textContent.toLowerCase().indexOf(filterText)!=-1){
+//     //     item.style.display = 'block';
+//     // }else{
+//     //     item.style.display = 'none';
+//     // }
+//    })
+for(let i=0;i<itemText.length;i++){
+    if(itemText[i].textContent.toLowerCase().indexOf(filterText)!=-1){
+        itemText[i].style.display = 'block'
+    }else{
+        itemText[i].style.display = 'none'
+    }
+}
 }
