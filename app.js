55fd7a8e9476edbb2a0b82c8a8f6a97095f82bec
iqbalsalik <sisalik84@gmail.com>
2023-03-10 01:03:54 +0530

    Edit button

--- app.js
+++ app.js
@@ -12,13 +12,27 @@ function addUserList(e){
     //creating li name and email id given by user and appending in ul
     let li = document.createElement("li");
     li.className = "item";
-    li.textContent = `${inputName.value}:- email id: ${inputEmail.value}, Phone Number: ${phoneNumber.value} `
+    li.textContent = `${inputName.value}: ${inputEmail.value}:${phoneNumber.value}:`
+
+    //createing delete button
     let deletBtn =  document.createElement("button")
     deletBtn.textContent = "Delete";
     deletBtn.id = "delete"
     deletBtn.style.backgroundColor = "#f4f4f4";
     deletBtn.style.color = 'black'
     li.appendChild(deletBtn)
+
+    //creating edit button;
+    let editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.id = "edit";
+    editBtn.style.backgroundColor = "#f4f4f4";
+    editBtn.style.color = "black";
+    editBtn.style.marginLeft = "4px"
+    li.appendChild(editBtn)
+
+
+    //appending the li in ul
     userList.appendChild(li)
 
     //setting localstorage
@@ -35,9 +49,21 @@ function addUserList(e){
 
     deletBtn.addEventListener("click",(e)=>{
         if(e.target.id.contains="delete"){
-            console.log(e.target.parentElement.textContent.split(":"))
+            console.log(e.target.parentElement.textContent)
             e.target.parentElement.remove()
             localStorage.removeItem(JSON.stringify(e.target.parentElement.textContent.split(":")[0]))
         }
     })
+
+    editBtn.onclick= (e)=>{
+        if(e.target.id.contains="edit"){
+            let targetData = e.target.parentElement.textContent.split(":")
+            e.target.parentElement.remove()
+            localStorage.removeItem(JSON.stringify(targetData[0]))
+            inputName.value = targetData[0];
+            inputEmail.value = targetData[1];
+            phoneNumber.value = targetData[2]
+            console.log(targetData)
+        }
+    }
 }
