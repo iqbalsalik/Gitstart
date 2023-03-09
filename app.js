8f8a9e84b5a3070f88931e0d1104b48e8b97ba98
iqbalsalik <sisalik84@gmail.com>
2023-03-10 00:28:32 +0530

    delete localstorage as with dom

--- app.js
+++ app.js
@@ -12,15 +12,32 @@ function addUserList(e){
     //creating li name and email id given by user and appending in ul
     let li = document.createElement("li");
     li.className = "item";
-    li.textContent = `${inputName.value} :- email id: ${inputEmail.value}, Phone Number: ${phoneNumber.value} `
+    li.textContent = `${inputName.value}:- email id: ${inputEmail.value}, Phone Number: ${phoneNumber.value} `
+    let deletBtn =  document.createElement("button")
+    deletBtn.textContent = "Delete";
+    deletBtn.id = "delete"
+    deletBtn.style.backgroundColor = "#f4f4f4";
+    deletBtn.style.color = 'black'
+    li.appendChild(deletBtn)
     userList.appendChild(li)
 
     //setting localstorage
-    let phoneValue = JSON.stringify(phoneNumber.value)
     let nameValue = JSON.stringify(inputName.value);
-    let emailValue = JSON.stringify(inputEmail.value);
-    localStorage.setItem(emailValue,[nameValue,phoneValue]) //serializing the local storage;
+    let userDetails = {
+        phone_Number : phoneNumber.value,
+        email_id : inputEmail.value
+    }
+    localStorage.setItem(nameValue,JSON.stringify(userDetails)) //serializing the local storage;
 
     inputEmail.value = ''
     inputName.value = ''
+    phoneNumber.value = ''
+
+    deletBtn.addEventListener("click",(e)=>{
+        if(e.target.id.contains="delete"){
+            console.log(e.target.parentElement.textContent.split(":"))
+            e.target.parentElement.remove()
+            localStorage.removeItem(JSON.stringify(e.target.parentElement.textContent.split(":")[0]))
+        }
+    })
 }
